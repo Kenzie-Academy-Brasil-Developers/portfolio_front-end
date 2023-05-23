7d23d8c6c0374578d1136312a2821983d19f5037
unknown <marcelinogs2001@gmail.com>
2023-05-23 18:40:00 -0300

    finish

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,20 +2,18 @@ import { SiGithub } from "react-icons/si";
 
 export function ProjectCard({ projectName, projectDescription, projectLink }) {
   return (
-    <>
-      <div className="p-5 border-white/30 border-2 rounded-2xl flex gap-3 relative">
-        <SiGithub className="absolute top-3.5 right-3.5 fill-slate-50 text-3xl" />
-        <div className="flex flex-col gap-3 mr-10 mb-10">
-          <h1 className="text-2xl font-bold self-start">{projectName}</h1>
-          <p>{projectDescription}</p>
-        </div>
-        <a
-          className="text-primary link-hover absolute bottom-3.5 left-4.5 w-fit"
-          href={projectLink}
-        >
-          Live demo
-        </a>
+    <div className="p-5 border-white/30 border-2 rounded-2xl flex gap-3 relative">
+      <SiGithub className="absolute top-3.5 right-3.5 fill-slate-50 text-3xl" />
+      <div className="flex flex-col gap-3 mr-10 mb-10">
+        <h1 className="text-2xl font-bold self-start">{projectName}</h1>
+        <p>{projectDescription}</p>
       </div>
-    </>
+      <a
+        className="text-primary link-hover absolute bottom-3.5 left-4.5 w-fit"
+        href={projectLink}
+      >
+        Live demo
+      </a>
+    </div>
   );
 }
--- src/components/StackCard.jsx
+++ src/components/StackCard.jsx
@@ -1,8 +1,12 @@
+import Tilt from "react-parallax-tilt";
+
 export function StackCard({ imgSrc, technology }) {
   return (
-    <div className="p-5 border-white/30 border-2 rounded-2xl flex gap-3 items-center">
-      <img style={{ height: "20px" }} src={imgSrc} alt={technology} />
-      <span className="text-lg font-bold">{technology}</span>
-    </div>
+    <Tilt>
+      <div className="p-5 border-white/30 border-2 rounded-2xl flex gap-3 items-center">
+        <img style={{ height: "20px" }} src={imgSrc} alt={technology} />
+        <span className="text-lg font-bold">{technology}</span>
+      </div>
+    </Tilt>
   );
 }
