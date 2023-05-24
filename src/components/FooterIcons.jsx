8c1fee8df4fc4d641298be6e0a705f5fd0c94f24
unknown <marcelinogs2001@gmail.com>
2023-05-24 09:38:34 -0300

    fix footer button color

--- src/components/FooterIcons.jsx
+++ src/components/FooterIcons.jsx
@@ -1,6 +1,6 @@
 export function FooterIcon({ children, link }) {
   return (
-    <li className="btn btn-outline border-none rounded-full">
+    <li className="btn btn-outline btn-primary border-none rounded-full">
       <a href={link} target="_blank">
         {children}
       </a>
