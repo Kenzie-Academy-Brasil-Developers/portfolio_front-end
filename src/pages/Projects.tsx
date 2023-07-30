d9c9d2b80c396098cd0a8f1df38991d8d64a8606
unknown <marcelinogs2001@gmail.com>
2023-07-30 17:27:16 -0300

    fix typo

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -31,8 +31,8 @@ export const Projects = () => {
             title="Dev Movies"
             description={
               translation
-                ? "Dev Movies é uma plataforma fullstack que permite aos usuários se inscreverem e publicarem avaliações de filmes na própria plataforma. Também podem ver as avaliações de outros usuários. Existe a possibilidade de criar, editar e excluir avaliações."
-                : "Dev Movies is a full-stack platform that allows users to sign up and post movie reviews on the platform itself, as well as view reviews from other users. There's also the option to create, edit, and delete reviews."
+                ? "Dev Movies é uma plataforma fullstack que permite aos usuários se inscreverem e publicarem suas avaliações de filmes. Também podem ver as avaliações de outros usuários assim como criar, editar e excluir suas próprias avaliações."
+                : "Dev Movies is a full-stack platform that allows users to sign up and post their own movie reviews on the platform, as well as view reviews from other users. You are able create, edit, and delete reviews."
             }
             reverse={false}
             technologies={[
